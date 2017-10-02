1ea8997c2c3cd4669fc563ea277ebe764d5568eb
stonar96 <minecraft.stonar96@gmail.com>
2017-10-02 22:15:29 +0200

    Fix scripts and format patches

--- format-patches.sh
+++ format-patches.sh
@@ -2,4 +2,4 @@
 rm -rf 'Patches'
 mkdir 'Patches'
 cd 'Minecraft-Alpha-Server'
-git format-patch -o '../Patches' "$(git rev-list --max-parents=0 HEAD)"
+git format-patch -o '../Patches' "$(git rev-list --max-parents=0 HEAD)" -N
